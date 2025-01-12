4a78354fe36c44afb0ec07ef9be8887b169a6a50
Stevie153 <ce-saowusu1320@st.umat.edu.gh>
2025-01-12 21:14:30 +0000

    added chnages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className=" flex my-20 justify-center">
-        <h1 className="text-4xl text-red-500">Welcome</h1>
+        <h1 className="text-2xl text-red-500 font-jost font-bold ">Welcome</h1>
       </div>
     </>
   );
